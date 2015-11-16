a96a70cd5b777d42072a878214daef0d8ae09890
srdja <srdja.panic@gmail.com>
2015-11-16 18:23:38 +0100

    Removing comments

--- src-cljs/raycaster_demo/ray.cljs
+++ src-cljs/raycaster_demo/ray.cljs
@@ -20,12 +20,8 @@
 
 
 (defn parent-quadrant
-  "Returns the parent quadrant of the [org] point and direction [dir]
-   If a point sits on a grid point the direction is used to determine
-   to which grid cell does the point belong"
   [org dir]
   (assoc quadrant
-         ;; Since a border point can belong to both quadrants
          :x (if (> (nth dir 0) 0)
               (int (nth org 0))
               (.ceil js/Math (- (nth org 0) 1)))
@@ -37,22 +33,14 @@
 
 
 (defn sub-quadrant
-  "Returns a sub-quadrant of a quadrant through which the ray
-  is traveling. The x and y of the sub-quadrant represent the
-  origin of the ray.
-
-  [f]    function that returns the quadrant of [org]
-  [org]  ray origin vector
-  [dir]  ray direction vector
-  "
   [paren org dir]
-  (let [org-x (nth org 0)
-        org-y (nth org 1)
-        width (if (< (nth dir 0) 0)                    ;; -x
-                 (* -1 (- org-x (:x paren)))
+  (let [org-x  (nth org 0)
+        org-y  (nth org 1)
+        width  (if (< (nth dir 0) 0)
+                 (- (:x paren) org-x)
                  (- (+ (:x paren) (:w paren)) org-x))
-        height (if (< (nth dir 1) 0)                   ;; -y
-                 (* -1 (- org-y (:y paren)))
+        height (if (< (nth dir 1) 0)
+                 (- (:y paren) org-y)
                  (- (+ (:y paren) (:h paren)) org-y))]
     (assoc quadrant
            :x org-x
@@ -61,17 +49,15 @@
            :h height)))
 
 
-;; FIXME - names
 (defn next-grid-intersect
-  "Returns the next point on the grid that the ray would pass"
   [quad ray-dir]
   (let [x (nth ray-dir 0)
         y (nth ray-dir 1)
         w (:w quad)
         h (:h quad)
         d (.sqrt js/Math (+ (* w w) (* h h)))
-        angle-a (.abs js/Math (.asin js/Math (/ h d))) ;; diagonal angle
-        angle-b (.abs js/Math (.asin js/Math y))]      ;; direction angle
+        angle-a (.abs js/Math (.asin js/Math (/ h d)))
+        angle-b (.abs js/Math (.asin js/Math y))]
     (let [scale (cond
                   (and (<= angle-b angle-a) (not= x 0)) (/ w x)
                   (and (>  angle-b angle-a) (not= y 0)) (/ h y)
@@ -81,7 +67,6 @@
 
 
 (defn end-grid-point
-  "Casts a ray across the grid and returns the end point"
   [map org dir]
   (loop [p org]
     (let [par (parent-quadrant p dir)]
